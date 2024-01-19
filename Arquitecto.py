9c437574076b758122ce3c00738234c363c704bf
Silvano <ticssa283@gmail.com>
2024-01-18 21:11:52 -0400

    inge_o

--- Arquitecto.py
+++ Arquitecto.py
@@ -5,3 +5,13 @@ costo = 0
 
 base = int(input("Dame el valor de la base "))
 altura = int(input("Dame el valor de la altura "))
+
+class Rectangulo:
+	 global area
+	 area = base * altura
+	 print(f"El area del terreno es de: {area} metros cuadrados")
+
+	
+class Costo:
+ costo = area * 2500
+ print("El costo a pagar por el terreno es de $" + str(costo))
